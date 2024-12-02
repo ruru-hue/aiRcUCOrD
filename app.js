c3ad85281d989a95c4df2682b1894ab51bd548df
ruru-hue <23hs126@s.bgu.ac.jp>
2024-12-02 13:44:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,23 +3,23 @@
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "凶"; // デフォルトの値
 
-            if (colour === "黒" && number === 0) {
+            if (colour === "青" && number === 0) {
                 result = "小吉";
-            } else if (colour === "黒" && number === 1) {
+            } else if (colour === "青" && number === 1) {
                 result = "中吉";
-            } else if (colour === "黒" && number === 2) {
+            } else if (colour === "青" && number === 2) {
                 result = "凶";
-            } else if (colour === "白" && number === 0) {
+            } else if (colour === "赤" && number === 0) {
                 result = "中吉";
-            } else if (colour === "白" && number === 1) {
+            } else if (colour === "赤" && number === 1) {
                 result = "中吉";
-            } else if (colour === "白" && number === 2) {
+            } else if (colour === "赤" && number === 2) {
                 result = "大凶";
-            } else if (colour === "オレンジ" && number === 0) {
+            } else if (colour === "黄色" && number === 0) {
                 result = "凶";
-            } else if (colour === "オレンジ" && number === 1) {
+            } else if (colour === "黄色" && number === 1) {
                 result = "大吉";
-            } else if (colour === "オレンジ" && number === 2) {
+            } else if (colour === "黄色" && number === 2) {
                 result = "中吉";
             }
 
